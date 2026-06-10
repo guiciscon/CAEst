ebf03655d92bc765abadcdc1f76c46f09a8f8c83
thdmarques <thiago.dmarques7@gmail.com>
2026-06-09 21:24:07 -0300

    Inicio de LiberacaomanualDao

--- caest/src/main/java/com/example/dao/LiberacaoManualDAO.java
+++ caest/src/main/java/com/example/dao/LiberacaoManualDAO.java
@@ -1,5 +1,33 @@
 package com.example.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.example.model.LiberacaoManual;
+import com.example.util.ConnectionFactory;
 
 public class LiberacaoManualDAO {
+
+    public void inserirLiberacaoManual(LiberacaoManual liberacao){
+        String sql = "INSERT INTO LiberacaoManual (docMotorista, motivo, dataHora, idVeiculo) VALUES (?, ?, ?, ?)";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, liberacao.getDocMotorista());
+            stmt.setString(2, liberacao.getMotivo());
+            //stmt.setDate(parameterIndex, x);(3, liberacao.getDataHora());
+            stmt.setInt(4, liberacao.getVeiculo().getId());
+            stmt.executeUpdate();
+
+            System.out.println("Liberação manual registrada!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    
+
+
 
 }
